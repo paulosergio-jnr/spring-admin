b872a481e13ef3bb57167bfaea7662c7b8686632
Paulo Sergio <paulosergio.jnr@gmail.com>
2019-03-25 22:55:58 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/SpringAdminApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableEurekaClient
+public class SpringAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminApplication.class, args);
+	}
+
+}
